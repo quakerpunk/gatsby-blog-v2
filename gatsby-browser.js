15f0230e63e1ddbd95660d93c1eef12bd279572e
Shawn Borton <shawn@shilohsvillage.com>
2023-01-26 15:12:55 -0600

    Initial Commit

---
+++ gatsby-browser.js
@@ -0,0 +1,11 @@
+// custom typefaces
+// import "@fontsource/montserrat/variable.css"
+// import "@fontsource/merriweather"
+import "@fontsource/merriweather-sans"
+// normalize CSS across browsers
+import "./src/normalize.css"
+// custom CSS styles
+import "./src/style.css"
+
+// Highlighting for code blocks
+import "prismjs/themes/prism.css"
